ab9e9c037b5bdcdb8c835593f942f7827a68e20c
unknown <wjdghcks02@gmail.com>
2024-03-01 17:09:42 +0900

    feat [ADD] ft_strtrim.c

---
+++ libft/srcs/ft_strtrim.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char    *ft_strtrim(char const *s1, char const *set)
+{
+    size_t  start;
+    size_t  end;
+    char    *result;
+
+    start = 0;
+    end = ft_strlen(s1) - 1;
+    while (s1[start] && ft_strrchr(set, s1[start]))
+        start++;
+    while (end && s1[end - 1] && ft_strchr(set, s1[end - 1]))
+        end--;
+    if (start > end)
+        return (ft_strdup(""));
+    result = ft_substr(s1, start, end - start);
+    return (result);
+}
