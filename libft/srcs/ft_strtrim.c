b7e55724e28a809e3ea73fc0b10316bade2ee68f
정호찬 <108447906+Eumgill98@users.noreply.github.com>
2024-03-03 15:15:17 +0900

    feat [FIX] ft_strtrim.c

--- libft/srcs/ft_strtrim.c
+++ libft/srcs/ft_strtrim.c
@@ -10,7 +10,7 @@ char    *ft_strtrim(char const *s1, char const *set)
     end = ft_strlen(s1);
     while (s1[start] && ft_strrchr(set, s1[start]))
         start++;
-    while (end && s1[end - 1] && ft_strchr(set, s1[end - 1]))
+    while (end >= 0 && ft_strchr(set, s1[end]))
         end--;
     if (start > end)
         return (ft_strdup(""));
