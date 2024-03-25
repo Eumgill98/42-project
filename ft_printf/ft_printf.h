ea4cb33926bafeeedf071e56e51e56a588b3f631
Hochan Jeong <hocjeong@c2r14s3.42gyeongsan.kr>
2024-03-25 16:58:34 +0900

    [ADD] ft_print init

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/25 14:22:04 by hocjeong          #+#    #+#             */
+/*   Updated: 2024/03/25 15:58:51 by hocjeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+
+#endif
