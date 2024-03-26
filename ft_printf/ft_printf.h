af08f60e5525186695ff0b041d965241d108fc7c
Hochan Jeong <hocjeong@c3r12s2.42gyeongsan.kr>
2024-03-26 17:25:09 +0900

    [MODIFY] ft_printf

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:22:04 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/03/25 15:58:51 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/03/26 15:12:00 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 # include <stdlib.h>
 # include <stdarg.h>
 # include <unistd.h>
+
+int	ft_printf(const char *format, ...);
 
 #endif
