4f6985e4401ded7519cfe181e0a356db96fd7a72
wjdghcks6735 <wjdghcks02@gmail.com>
2024-03-27 18:04:51 +0900

    [ADD] ft_printf sub function

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -12,62 +12,17 @@
 
 #include "ft_printf.h"
 
-int	_check_flag(const char *format)
+static int	_doprintf(va_list ap, const char *format)
 {
-	char	*check_flag = "cspdiuxX%";
-	int	flag;
+	int	printed;
 
-	flag = 0;
-	while (check_flag[flag])
-	{
-		if (check_flag[flag] == *format)
-			return (flag);
-		flag++;
-	}
-	return (-1);
-}
-
-int	_check_n_write(va_list ap, const char *format);
-{
-	int	read_size;
-
-	if (_check_flag(format + 1) == 0)
-		// write sigle Char
-	else if (_check_flag(format + 1) == 1)
-		// write String
-	else if (_check_flag(format + 1) == 2)
-		// write Pointer 
-	else if (_check_flag(format + 1) == 3)
-		// write d
-	else if (_check_flag(format + 1) == 4)
-		// write i
-	else if (_check_flag(format + 1) == 5)
-		// write u
-	else if (_check_flag(format + 1) == 6)
-		// write x
-	else if (_check_flag(format + 1) == 7)
-		// write X
-	else if (_check_flag(format + 1) == 8)
-		// write %
-	else if (_check_flag(format + 1) == -1)
-		// fuck
-	return (read_size);
-}
-
-int	_doprintf(va_list ap, const char *format)
-{
-	int	read_size;
-
-	read_size = 0;
+	printed = 0;
 	while (*format)
 	{
 		if (*format == '%' && *(format + 1))
-			read_size += _check_n_write(ap, format);
+			printed += check_n_write(ap, format);
 		else
-		{
-			write(1, format, 1);
-			read_size++;
-		}
+			printed += write(1, format, 1);
 		format++;
 	}
 	return (read_size);
@@ -76,11 +31,11 @@ int	_doprintf(va_list ap, const char *format)
 int	ft_printf(const char *format, ...)
 {
 	va_list	ap;
-	int	read_size;
+	int	printed;
 
-	read_size = 0;
+	printed = 0;
 	va_start(ap, format);
-	read_size = _doprintf(ap, format);
+	printed = _doprintf(ap, format);
 	va_end(ap);
-	return (read_size);
+	return (printed);
 }
--- ft_printf/ft_printf_str.c
+++ ft_printf/ft_printf_str.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf_str.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -10,35 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdlib.h>
-# include <stdarg.h>
-# include <unistd.h>
+int ft_print_char(va_list ap)
+{
+    char    c;
 
-//printf
-int	ft_printf(const char *format, ...);
+    c = (unsigned char)va_arg(ap, int);
+    return (write(1, &c, 1));
+}
 
-//utils
-int check_n_write(va_list ap, const char *format);
+int ft_print_str(va_list ap)
+{
+    char    *s;
+    int printed;
 
-//print_str.c
-int ft_print_char(va_list ap);
-int ft_print_str(va_list ap);
-
-//print_num.c
-int ft_print_digit(va_list ap);
-int ft_print_int(va_list ap);
-int ft_print_unint(va_list ap);
-
-//print_hex family
-int ft_print_ptr(va_list ap);
-int ft_print_s_hex(va_list ap);
-int ft_print_b_hex(va_list ap);
-
-//print_percent & other
-int ft_print_percent(va_list ap);
-int ft_print_other(const char *format);
-
-#endif
+    s = va_arg(ap, char *);
+    printed = 0;
+    while (!s)
+    {
+        printed += write(1, s, 1);
+        s++;
+    }
+    return (printed)
+}
