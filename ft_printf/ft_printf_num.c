cfd86f90ad7f2e45db26855c4dc21f5e386717d9
Hochan Jeong <hocjeong@c3r18s3.42gyeongsan.kr>
2024-03-28 17:17:33 +0900

    [ADD] int, unint print function

--- ft_printf/ft_printf_num.c
+++ ft_printf/ft_printf_num.c
@@ -6,11 +6,54 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:22:04 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/03/28 16:41:56 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:15:01 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_putnbr_int(int num)
+{
+	int	printed;
+
+	printed = 0;
+	if (n == -2147483648)
+		printed += write(1, "-2147483648", 11);
+	else if (num < 0)
+	{
+		printed += write(1, "-", 1);
+		printed += ft_putnbr(-num, printed);
+	}
+	else if (num < 10 && num >= 0)
+	{
+		num = num + '0';
+		printed += write(1, &num, 1);
+	}
+	else
+	{
+		printed += ft_putnbr(num / 10);
+		printed += ft_putnbr(num % 10);
+	}
+	return (printed);
+}
+
+static int	ft_putnbr_unit(unsigned int num)
+{
+	int	printed;
+
+	printed = 0;
+	if (num < 10 && num >= 0)
+	{
+		num = num + '0';
+		printed += write(1, &num, 1);
+	}
+	else
+	{
+		printed += ft_putnbr(num / 10);
+		printed += ft_putnbr(num % 10);
+	}
+	return (printed);
+}
 
 int	ft_print_digit(va_list ap)
 {
@@ -26,16 +69,16 @@ int	ft_print_int(va_list ap)
 	int	printed;
 
 	num = va_arg(ap, int);
-	// printed = ft_print_num(num);
+	printed = ft_putnbr_int(num);
 	return (printed);
 }
 
 int	ft_print_unint(va_list ap)
 {
 	unsigned int	num;
-	int	printed;
+	int				printed;
 
 	num = va_arg(ap, unsigned int);
-	// printed = ft_print_num(num);
+	printed = ft_putnbr_unit(num);
 	return (printed);
 }
--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 14:22:04 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/03/28 16:47:33 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/03/28 17:15:17 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,11 @@ int	check_n_write(va_list ap, const char *format)
 	else if (_check_flag(format + 1, "cspdiuxX%") == 2)
 		// printed = ft_print_ptr();
 	else if (_check_flag(format + 1, "cspdiuxX%") == 3)
-		printed = ft_print_digit();
+		printed = ft_print_digit(ap);
 	else if (_check_flag(format + 1, "cspdiuxX%") == 4)
-		// printed = ft_print_int();
+		printed = ft_print_int(ap);
 	else if (_check_flag(format + 1, "cspdiuxX%") == 5)
-		// printed = ft_print_unint();
+		printed = ft_print_unint(ap);
 	else if (_check_flag(format + 1, "cspdiuxX%") == 6)
 		// printed = ft_print_s_hex();
 	else if (_check_flag(format + 1, "cspdiuxX%") == 7)
