1294b77e1296b0386ebe080b6ccddf9c1cb67372
Hochan Jeong <hocjeong@c2r13s1.42gyeongsan.kr>
2024-03-29 16:30:24 +0900

    [ADD] print hex function

---
+++ ft_printf/ft_printf_hex.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_hex.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 16:02:30 by hocjeong          #+#    #+#             */
+/*   Updated: 2024/03/29 16:28:50 by hocjeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_putchar(int num)
+{
+	int	printed;
+
+	printed = write(1, &num, 1);
+	return (printed);
+}
+
+static int	ft_put_hex(unsigned int num, const char *refer)
+{
+	int	printed;
+
+	printed = 0;
+	if (num >= 16)
+	{
+		printed += ft_put_hex(num / 16, refer);
+		printed += ft_put_hex(num % 16, refer);
+	}
+	else
+		printed += ft_putchar(refer[num]);
+	return (printed);
+}
+
+int	ft_print_s_hex(va_list ap)
+{
+	unsigned int	num;
+	int				printed;
+
+	num = va_arg(ap, unsigned int);
+	printed = ft_put_hex(num, "0123456789abcdef");
+	return (printed);
+}
+
+int	ft_print_b_hex(va_list ap)
+{
+	unsigned int	num;
+	int				printed;
+
+	num = va_arg(ap, unsigned int);
+	printed = ft_put_hex(num, "012345789ABCDEF");
+	return (printed);
+}
+
+int	ft_print_ptr(va_list ap)
+{
+	void	*ptr;
+	int		printed;
+
+	ptr = va_arg(ap, void *);
+	printed = ft_put_hex((unsigned long long)ptr, "0123456789abcdef");
+	return (printed);
+}
