1940680f3e497da306eaa2af8f870f2fff1c3e68
Hochan Jeong <hocjeong@c2r14s3.42gyeongsan.kr>
2024-05-16 18:37:22 +0900

    [ADD] add map_check function - add `map_check` function skeleton - norm error of so_long .h

--- so_long/map_check.c
+++ so_long/map_check.c
@@ -6,8 +6,18 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:47:57 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/05/16 17:47:59 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/05/16 18:36:21 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+int	map_check(t_sets *sets)
+{
+	if (check_rectangular(sets) == -1)
+		return (-1);
+	if (check_component(sets) == -1)
+		return (-1);
+	if (check_wall(sets) == -1)
+		return (-1);
+	//todo path check
+	return (0)
+}
