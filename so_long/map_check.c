5aafe0d7b67e0254ef13ba0827f56b6e22a4b864
Hochan Jeong <hocjeong@c2r14s3.42gyeongsan.kr>
2024-05-16 20:03:07 +0900

    [FIX] map_check

--- so_long/map_check.c
+++ so_long/map_check.c
@@ -6,15 +6,29 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:47:57 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/05/16 18:36:21 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/05/16 20:03:01 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "so_long.h"
+
+static int	check_map_flag(t_sets *sets)
+{
+	if (sets->map_flag->e_num != 1)
+		return (-1);
+	if (sets->map_flag->p_num != 1)
+		return (-1);
+	if (!(sets->map_flag->c_num))
+		return (-1);
+	return (0);
+}
 
 int	map_check(t_sets *sets)
 {
 	if (check_rectangular(sets) == -1)
 		return (-1);
 	if (check_component(sets) == -1)
+		return (-1);
+	if (check_map_flag(sets == -1)
 		return (-1);
 	if (check_wall(sets) == -1)
 		return (-1);
