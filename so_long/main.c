d1d76f7be2822fc45ea8680bcf77744ec943ae35
wjdghcks6735 <wjdghcks02@gmail.com>
2024-05-18 16:29:25 +0900

    [ADD] img file

--- so_long/main.c
+++ so_long/main.c
@@ -11,19 +11,36 @@
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <stdio.h>
-int	main(void)
+
+static int	check_extension(const char *filename, const char *extension)
+{
+	int filename_len;
+	int	extension_len;
+
+	filename_len = ft_strlen(filename);
+	extension_len = ft_strlen(filename);
+	if (filename_len < extension_len) 
+		return (-1);
+	if (ft_strncmp(filename + filename_len - extension_len, extension, extension_len))
+		return (-1);
+	return (0);
+}
+
+int	main(int ac, char **av)
 {
 	t_sets	sets;
 
-	init_sets(&sets, "map/map1.ber");
-	load_map(&sets);
-	printf("row : %d\n", sets.row);
-	printf("fd : %d\n", sets.fd);
+	if (ac == 3)
+	{
+		if (check_extension(av[2], ".ber") == -1)
+			return (-1);
 
-	sets.mlx_ptr = mlx_init();
-	sets.win_ptr = mlx_new_window(sets.mlx_ptr, 600, 600, "my_mlx");
-	mlx_key_hook(sets.win_ptr, esc_hook, &sets);
-	mlx_loop(sets.mlx_ptr);
+		init_sets(&sets, av[2]);
+		load_map(&sets);
+		sets.mlx_ptr = mlx_init();
+		sets.win_ptr = mlx_new_window(sets.mlx_ptr, 600, 600, "my_mlx");
+		mlx_key_hook(sets.win_ptr, esc_hook, &sets);
+		mlx_loop(sets.mlx_ptr);
+	}
 	return (0);
 }
