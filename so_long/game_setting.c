45347846926cc7a21c0276ed7c7186292d07c078
Hochan Jeong <hocjeong@c3r11s6.42gyeongsan.kr>
2024-05-21 20:29:53 +0900

    [ADD] v.1.0

--- so_long/game_setting.c
+++ so_long/game_setting.c
@@ -12,35 +12,48 @@
 
 #include "so_long.h"
 
-static int	open_map_file(t_sets *sets, char *map_path)
+static int	load_resource_image(t_sets *sets)
 {
-	sets->fd = open(map_path, O_RDONLY);
-	if (sets->fd < 0)
+	sets->imgs.g = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/0.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.g)
+		return (-1);
+	sets->imgs.w = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/1.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.w)
+		return (-1);
+	sets->imgs.c = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/c.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.c)
+		return (-1);
+	sets->imgs.e = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/e.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.e)
+		return (-1);
+	sets->imgs.e_open = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/e_open.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.e_open)
 		return (-1);
 	return (0);
 }
 
-static int	load_image_file(t_sets *sets)
+static int	load_player_image(t_sets *sets)
 {
-	sets->g = mlx_xpm_file_to_image(sets->mlx_ptr, \
-		"img/0.xpm", &sets->i_w, &sets->i_h);
-	if (!sets->g)
+	sets->imgs.back = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/back.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.back)
 		return (-1);
-	sets->w = mlx_xpm_file_to_image(sets->mlx_ptr, \
-		"img/1.xpm", &sets->i_w, &sets->i_h);
-	if (!sets->w)
+	sets->imgs.right = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/right.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.right)
 		return (-1);
-	sets->c = mlx_xpm_file_to_image(sets->mlx_ptr, \
-		"img/c.xpm", &sets->i_w, &sets->i_h);
-	if (!sets->c)
+	sets->imgs.front = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/front.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.front)
 		return (-1);
-	sets->e = mlx_xpm_file_to_image(sets->mlx_ptr, \
-		"img/e.xpm", &sets->i_w, &sets->i_h);
-	if (!sets->e)
-		return (-1);
-	sets->p = mlx_xpm_file_to_image(sets->mlx_ptr, \
-		"img/p.xpm", &sets->i_w, &sets->i_h);
-	if (!sets->p)
+	sets->imgs.left = mlx_xpm_file_to_image(sets->mlx_ptr, \
+		"img/left.xpm", &sets->imgs.i_w, &sets->imgs.i_h);
+	if (!sets->imgs.left)
 		return (-1);
 	return (0);
 }
@@ -54,10 +67,8 @@ static void	initialize_sets(t_sets *sets)
 	sets->visited = NULL;
 	sets->row = 0;
 	sets->col = 0;
-	sets->player_r = 0;
-	sets->player_c = 0;
-	sets->i_h = 32;
-	sets->i_w = 32;
+	sets->imgs.i_h = 0;
+	sets->imgs.i_w = 0;
 	sets->w_w = 0;
 	sets->w_h = 0;
 	sets->map_flag.e_num = 0;
@@ -65,15 +76,31 @@ static void	initialize_sets(t_sets *sets)
 	sets->map_flag.c_num = 0;
 }
 
+static void	initialize_player_info(t_sets *sets)
+{
+	sets->player.player_r = 0;
+	sets->player.player_c = 0;
+	sets->player.collected = 0;
+	sets->player.direction = 2;
+	sets->player.door_r = 0;
+	sets->player.door_c = 0;
+	sets->player.door_open = 0;
+	sets->player.movement = 0;
+}
+
 int	init_sets(t_sets *sets, char *map_path)
 {
 	initialize_sets(sets);
-	if (open_map_file(sets, map_path) == -1)
+	initialize_player_info(sets);
+	sets->fd = open(map_path, O_RDONLY);
+	if (sets->fd < 0)
 		return (-1);
 	sets->mlx_ptr = mlx_init();
 	if (!sets->mlx_ptr)
 		return (-1);
-	if (load_image_file(sets) == -1)
+	if (load_resource_image(sets) == -1)
+		return (-1);
+	if (load_player_image(sets) == -1)
 		return (-1);
 	return (0);
 }
--- so_long/map_draw.c
+++ so_long/map_draw.c
@@ -12,22 +12,46 @@
 
 #include "so_long.h"
 
-static int	img_draw(t_sets *sets, char flag, int x, int y)
+int	player_draw(t_sets *sets, int x, int y)
 {
-	mlx_put_image_to_window(\
-		sets->mlx_ptr, sets->win_ptr, sets->g, x, y);
+	if (sets->player.direction == 0)
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.back, x, y);
+	else if (sets->player.direction == 1)
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.right, x, y);
+	else if (sets->player.direction == 2)
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.front, x, y);
+	else
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.left, x, y);
+	return (0);
+}
+
+int	img_draw(t_sets *sets, char flag, int x, int y)
+{
 	if (flag == '1')
 		mlx_put_image_to_window(\
-			sets->mlx_ptr, sets->win_ptr, sets->w, x, y);
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.w, x, y);
 	else if (flag == 'P')
-		mlx_put_image_to_window(\
-			sets->mlx_ptr, sets->win_ptr, sets->p, x, y);
+		player_draw(sets, x, y);
 	else if (flag == 'C')
 		mlx_put_image_to_window(\
-			sets->mlx_ptr, sets->win_ptr, sets->c, x, y);
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.c, x, y);
 	else if (flag == 'E')
+	{
 		mlx_put_image_to_window(\
-			sets->mlx_ptr, sets->win_ptr, sets->e, x, y);
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.e, x, y);
+		sets->player.door_r = y / sets->imgs.i_w;
+		sets->player.door_c = x / sets->imgs.i_h;
+	}
+	else if (flag == 'O')
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.e_open, x, y);
+	else
+		mlx_put_image_to_window(\
+			sets->mlx_ptr, sets->win_ptr, sets->imgs.g, x, y);
 	return (0);
 }
 
@@ -43,7 +67,7 @@ int	map_draw(t_sets *sets)
 		while (sets->map[row][col])
 		{
 			img_draw(sets, sets->map[row][col], \
-				col * sets->i_w, row * sets->i_h);
+				col * sets->imgs.i_w, row * sets->imgs.i_h);
 			col++;
 		}
 		row++;
