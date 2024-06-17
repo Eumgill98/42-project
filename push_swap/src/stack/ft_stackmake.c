8d0a43b685d0a366ff861cf42e392f206a7baae3
Hochan Jeong <hocjeong@c3r17s6.42gyeongsan.kr>
2024-06-17 17:13:21 +0900

    add : push_swap input check functions

--- push_swap/src/stack/ft_stackmake.c
+++ push_swap/src/stack/ft_stackmake.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_stackmake.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/11 16:45:22 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/17 16:37:51 by hocjeong         ###   ########.fr       */
+/*   Created: 2024/06/11 18:58:27 by hocjeong          #+#    #+#             */
+/*   Updated: 2024/06/17 17:09:47 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-
 #include "stack.h"
 
-#endif
+int	ft_stackmake(t_stack *stack, char **s)
+{
+	int	idx;
+
+	if (!s || !stack)
+		return (-1);
+	idx = 0;
+	while (s[idx])
+	{
+		if (ft_dlstadd_back(stack, s[idx]) == -1)
+		{
+			ft_dlstpop_all(stack);
+			return (-1);
+		}
+		idx++;
+	}
+	return (0);
+}
