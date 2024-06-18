ed9ee780a8126bb26349f364965f9833b1979e70
Hochan Jeong <hocjeong@c2r13s3.42gyeongsan.kr>
2024-06-18 13:52:28 +0900

    add : check input form and check input dup

--- push_swap/src/input_utils.c
+++ push_swap/src/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 16:37:03 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/17 17:10:32 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/06/18 13:51:17 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,29 @@ static size_t	ft_dstrlen(char **s)
 	while (s[len])
 		len++;
 	return (len);
-}	
+}
+
+static int	check_duplication(char *s1, char *s2)
+{
+	if (ft_atoi(s1) == ft_atoi(s2))
+		return (-1);
+	return (0);
+}
+
+static int	check_wrong_form(char *s)
+{
+	int	idx;
+
+	idx = 0;
+	while (s[idx])
+	{
+		if (!((*s >= '0' && *s <= '9') \
+			|| *s == '+' || *s == '-'))
+			return (-1);
+		idx++;
+	}
+	return (0);
+}
 
 int	check_input(char **s)
 {
@@ -34,10 +56,13 @@ int	check_input(char **s)
 	i = 0;
 	while (i < len)
 	{
+		if (check_wrong_form(s[j]) == -1)
+			return (-1);
 		j = i + 1;
 		while (s[j])
 		{
-			if (ft_atoi(s[i]) == ft_atoi(s[j]))
+			if (check_duplication(s[i], s[j]) == -1 \
+				|| check_wrong_form(s[j]) == -1)
 				return (-1);
 			j++;
 		}
