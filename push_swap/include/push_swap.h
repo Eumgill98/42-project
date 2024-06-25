353297e45f7a784838ec4c0f554375cce0988e4a
Hochan Jeong <hocjeong@c3r17s6.42gyeongsan.kr>
2024-06-25 16:22:44 +0900

    [MOD] change name of files

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:45:22 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/17 16:37:51 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/06/25 16:22:25 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define PUSH_SWAP_H
 
 #include "stack.h"
+
+int	ps_input(char **s, int len);
 
 #endif
--- push_swap/src/ps_input.c
+++ push_swap/src/ps_input.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   input_utils.c                                      :+:      :+:    :+:   */
+/*   ps_input.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 16:37:03 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/18 13:51:17 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/06/25 16:22:23 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static size_t	ft_dstrlen(char **s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
 
 static int	check_duplication(char *s1, char *s2)
 {
@@ -44,29 +34,7 @@ static int	check_wrong_form(char *s)
 	return (0);
 }
 
-int	check_input(char **s)
+int	ps_input(char **s, int len)
 {
-	size_t	len;
-	size_t	i;
-	size_t	j;
-
-	if (!s || !(*s))
-		return (-1);
-	len = ft_dstrlen(s);
-	i = 0;
-	while (i < len)
-	{
-		if (check_wrong_form(s[j]) == -1)
-			return (-1);
-		j = i + 1;
-		while (s[j])
-		{
-			if (check_duplication(s[i], s[j]) == -1 \
-				|| check_wrong_form(s[j]) == -1)
-				return (-1);
-			j++;
-		}
-		i++;
-	}
-	return (len);
+	// 부호 
 }
