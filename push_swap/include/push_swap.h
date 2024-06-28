679a597e77b8e66211b8ecf39fa46d83a42d7245
Hochan Jeong <hocjeong@c3r17s4.42gyeongsan.kr>
2024-06-28 21:47:02 +0900

    [ADD] ps_input check functions

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,20 +6,21 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:45:22 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/28 18:08:33 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/06/28 21:20:41 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "stack.h"
+# include "stack.h"
 
 char	**ps_input(int ac, char **av);
 
-int	check_overflow(char **s);
-int	check_dup(char **s);
-int	check_form(char **s);
+int		check_overflow(char **s);
+int		check_dup(char **s);
+int		check_form(char **s);
 
+int		ft_atoil(const char *nptr, int *overflow);
 
 #endif
--- push_swap/src/ps_input_utils.c
+++ push_swap/src/ps_input_utils.c
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 18:05:47 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/06/28 18:13:38 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/06/28 21:46:36 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,63 @@
 
 int	check_overflow(char **s)
 {
+	int	overflow;
+	int	idx;
+
+	overflow = 0;
+	idx = 0;
+	while (s[idx])
+	{
+		ft_atoil(s[idx], &overflow);
+		if (overflow)
+			return (-1);
+		idx++;
+	}
+	return (0);
 }
 
 int	check_dup(char **s)
 {
+	int	i;
+	int	j;
+	int	now;
+
+	i = 0;
+	while (s[i])
+	{
+		j = i + 1;
+		now = ft_atoi(s[i]);
+		while (s[j])
+		{
+			if (now == ft_atoi(s[j]))
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
 
 int	check_form(char **s)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s[i])
+	{
+		j = 0;
+		if (s[i][j] == '-' || s[i][j] == '+')
+			j++;
+		if (!s[i][j])
+			return (-1);
+		while (s[i][j])
+		{
+			if !(s[i][j] >= '0' || s[i][j] <= '9')
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
