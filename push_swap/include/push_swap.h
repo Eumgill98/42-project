5fc143faa9f7ef4025e122674f332ffbf1adcaae
Hochan Jeong <hocjeong@c3r17s2.42gyeongsan.kr>
2024-07-11 15:47:16 +0900

    [FIX] push_swap norm error

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:45:22 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/09 17:05:11 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/11 15:47:02 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ void		ps_free_stacks(t_ps_stacks *stacks);
 void		ps_free_input(char **s);
 
 int			ps_encoding(const char *command);
-int 		ps_functions(t_ps_stacks *stacks, const char *command);
+int			ps_functions(t_ps_stacks *stacks, const char *command);
 
 int			ps_sort_check(t_stack *stack);
 int			ps_rsort_check(t_stack *stack);
--- push_swap/src/stack/ft_stackcopy.c
+++ push_swap/src/stack/ft_stackcopy.c
@@ -6,29 +6,29 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:14:13 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/04 17:19:00 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/11 15:38:27 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
 
-t_stack *ft_stackcopy(t_stack *origin_stack)
+t_stack	*ft_stackcopy(t_stack *origin_stack)
 {
-    t_stack *new_stack;
-    t_dlst  *tmp;
+	t_stack	*new_stack;
+	t_dlst	*tmp;
 
-    new_stack = ft_stackinit();
-    if (!new_stack)
-        return (NULL);
-    tmp = origin_stack->head;
-    while(tmp)
-    {
-        if (ft_dlstadd_back(new_stack, tmp->element) == -1)
-        {
-            ft_dlstpop_all(new_stack);
-            return(NULL);
-        }
-        tmp = tmp->next;
-    }
-    return (new_stack);
+	new_stack = ft_stackinit();
+	if (!new_stack)
+		return (NULL);
+	tmp = origin_stack->head;
+	while (tmp)
+	{
+		if (ft_dlstadd_back(new_stack, tmp->element) == -1)
+		{
+			ft_dlstpop_all(new_stack);
+			return (NULL);
+		}
+		tmp = tmp->next;
+	}
+	return (new_stack);
 }
