54bd0ef64ce6ceec1bb48af6ba932b4ed5ae0d5b
Hochan Jeong <hocjeong@c3r18s6.42gyeongsan.kr>
2024-07-24 19:45:58 +0900

    [ADD] pipex env find and search

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:12:33 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/24 16:53:03 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/24 19:25:47 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,18 @@
 # include <stdio.h>
 # include "libft.h"
 
+typedef struct s_pipeinfo
+{
+	char	**commands;
+	char	**envs;
+}	t_pipeinfo;
+
 void	pi_freedstr(char **commands, int idx);
 
 char	**pi_command(int ac, char **av);
 
-char	*pi_env(char **env);
+char	*pi_env_find(char **env);
 char	**pi_env_split(char *path);
+char	*pi_env_access(char **envs, char *command);
 
 #endif
