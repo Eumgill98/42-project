af3c0f4d6d0d50bc188e44e5c567c5cddc686f89
Hochan Jeong <hocjeong@c3r18s6.42gyeongsan.kr>
2024-07-24 20:05:06 +0900

    [ADD] pi_path_join functions

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:12:33 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/24 19:25:47 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/24 20:04:56 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	pi_freedstr(char **commands, int idx);
 
 char	**pi_command(int ac, char **av);
 
+char	*pi_path_join(char *s1, char *s2);
 char	*pi_env_find(char **env);
 char	**pi_env_split(char *path);
 char	*pi_env_access(char **envs, char *command);
