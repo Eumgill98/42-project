6e48371314bcb6b4fb13ef0332d50d6a290980ab
Hochan Jeong <hocjeong@c3r18s6.42gyeongsan.kr>
2024-07-27 16:45:32 +0900

    [FIX] free bug

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 16:12:33 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/24 20:15:55 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/27 16:42:50 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ typedef struct s_pipeinfo
 	char	**envs;
 }	t_pipeinfo;
 
-void	pi_exit(t_pipeinfo *info);
+t_pipeinfo	*pi_init_info(int ac, char **av, char **env);
+void		pi_exit(t_pipeinfo *info);
 
-void	pi_freedstr(char **dstr, int idx);
-void	pi_freeinfo(t_pipeinfo *info);
+void		pi_freedstr_idx(char **dstr, int idx);
+void		pi_freedstr(char **dstr);
+void		pi_freeinfo(t_pipeinfo *info);
 
-int		pi_init_info(int ac, char **av, char **env, t_pipeinfo *info);
+char		**pi_command(int ac, char **av);
 
-char	**pi_command(int ac, char **av);
-
-char	*pi_path_join(char *s1, char *s2);
-char	*pi_env_find(char **env);
-char	**pi_env_split(char *path);
-char	*pi_env_access(char **envs, char *command);
+char		*pi_path_join(char *s1, char *s2);
+char		*pi_env_find(char **env);
+char		**pi_env_split(char *path);
+char		*pi_env_access(char **envs, char *command);
 
 #endif
