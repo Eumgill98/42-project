b696c26fa2725185ffc6e70ebc58e80e1ecd9667
Hochan Jeong <hocjeong@c3r17s4.42gyeongsan.kr>
2024-07-29 21:41:07 +0900

    [ADD] wait bug

--- pipex/src/pi_child.c
+++ pipex/src/pi_child.c
@@ -6,7 +6,7 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 15:44:48 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/07/29 17:32:37 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/07/29 21:40:57 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,8 @@ int	pi_child(t_pipeinfo *info, t_file *file, int *fd)
 			pi_middle_child(info, fd, idx);
 		else if (pid == 0 && idx == total_c - 1)
 			pi_tail_child(info, file, fd, idx);
-		else
-			waitpid(pid, NULL, 0);
 		idx++;
 	}
+	waitpid(-1, NULL, 0);
 	return (0);
 }
