6e6d01390b2b4caa3f34d7b43bd7df9036560b3f
Hochan Jeong <hocjeong@c3r19s4.42gyeongsan.kr>
2024-09-04 18:24:19 +0900

    add : parsing functions

---
+++ philosophers/philo/philo.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 15:52:28 by hocjeong          #+#    #+#             */
+/*   Updated: 2024/09/04 18:19:58 by hocjeong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct s_philo
+{
+	int				id;
+	int				dead;
+	int				eat_count;
+	pthread_t		*thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}	t_philo;
+
+typedef struct s_program
+{
+	int		num_philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		end_point;
+	struct timeval	start_time;
+	t_philo	*philos;
+}	t_program;
+
+int	ph_parsing(int ac, char **av, t_program *info);
+
+#endif
