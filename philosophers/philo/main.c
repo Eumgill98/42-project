ba39ebeb7ccf40961c142e72b845d5192eef7fdf
Hochan Jeong <hocjeong@c2r5s4.42gyeongsan.kr>
2024-09-26 18:13:27 +0900

    add : project init - TODO 1. 시간 조절 - TODO 2. 누수

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,32 +6,11 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:15:16 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/09/26 16:59:04 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:06:08 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	ph_pthread_exit(t_program *info)
-{
-	int	total_end_pthread;
-	int	idx;
-
-	while (1)
-	{
-		total_end_pthread = 0;
-		idx = 0;
-		while (idx < info->num_philos)
-		{
-			pthread_mutex_lock(info->end_mutex);
-			total_end_pthread += info->philos[idx]->thread_end;
-			pthread_mutex_unlock(info->end_mutex);
-			idx++;
-		}
-		if (total_end_pthread == info->num_philos)
-			break ;
-	}
-}
 
 int	main(int ac, char **av)
 {
@@ -46,6 +25,5 @@ int	main(int ac, char **av)
 	}
 	ph_philo(&info);
 	ph_monitoring(&info);
-	ph_pthread_exit(&info);
 	ph_free_info(&info);
 }
