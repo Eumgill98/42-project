0dcb8463d91810a765b41139fc1bb4f00a7a3d64
Hochan Jeong <hocjeong@c2r5s4.42gyeongsan.kr>
2024-09-26 18:13:27 +0900

    add : project init - TODO 1. 시간 조절 - TODO 2. 누수

--- philosophers/philo/ph_monitoring.c
+++ philosophers/philo/ph_monitoring.c
@@ -6,17 +6,77 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:04:57 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/09/26 17:37:16 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/09/26 18:11:44 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	ph_monitoring(t_program *info)
+static int	check_all_eaten(t_program *info)
 {
-// 설계
-// TODO 1 종료 조건 탐색
-// TODO 1.1 다 일정 횟수 먹었는지
-// TODO 1.2 죽었는지
+	int		idx;
+	int		enough_eat;
+	t_philo	*philo;
+
+	idx = 0;
+	enough_eat = 0;
+	while (idx < info->num_philos)
+	{
+		philo = info->philos[idx];
+		pthread_mutex_lock(philo->eat_count_mutex);
+		if (philo->eat_count == info->end_point)
+			enough_eat += 1;
+		pthread_mutex_unlock(philo->eat_count_mutex);
+		idx++;
+	}
+	if (enough_eat == info->end_point)
+	{
+		pthread_mutex_lock(info->dead_mutex);
+		info->end_flag = 1;
+		pthread_mutex_unlock(info->dead_mutex);
+		return (1);
+	}
+	return (0);
 }
 
+static int	check_dead(t_program *info)
+{
+	int		idx;
+	t_philo	*philo;
+
+	idx = 0;
+	while (idx < info->num_philos)
+	{
+		philo = info->philos[idx];
+		pthread_mutex_lock(philo->last_eaten_mutex);
+		if ((ph_now_ms(info) - philo->last_eaten) \
+				>= info->time_to_die)
+		{
+			pthread_mutex_lock(info->print_mutex);
+			pthread_mutex_lock(info->dead_mutex);
+			info->end_flag = 1;
+			printf("%lu %d is died\n", ph_now_ms(info), idx + 1);
+			pthread_mutex_unlock(info->print_mutex);
+			pthread_mutex_unlock(info->dead_mutex);
+			pthread_mutex_unlock(philo->last_eaten_mutex);
+			return (1);
+		}
+		pthread_mutex_unlock(philo->last_eaten_mutex);
+		idx++;
+	}
+	return (0);
+}
+
+void	ph_monitoring(t_program *info)
+{
+	int	all_eat_flag;
+	int	dead_flag;
+
+	while (1)
+	{
+		all_eat_flag = check_all_eaten(info);
+		dead_flag = check_dead(info);
+		if (all_eat_flag || dead_flag)
+			break ;
+	}
+}
