ac15f26e8acf84fee04244bb250fe266cce90e2a
Hochan Jeong <hocjeong@c2r9s6.42gyeongsan.kr>
2024-10-16 17:07:19 +0900

    add : mutual exclusion

--- philosophers/philo/ph_monitoring.c
+++ philosophers/philo/ph_monitoring.c
@@ -6,57 +6,76 @@
 /*   By: hocjeong <hocjeong@student.42gyeongsa      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 17:04:57 by hocjeong          #+#    #+#             */
-/*   Updated: 2024/10/16 16:08:47 by hocjeong         ###   ########.fr       */
+/*   Updated: 2024/10/16 16:49:43 by hocjeong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	check_end(t_program *info, int *eat)
+static int	check_end(t_program *info, int *eat, int *dead_idx)
 {
-	int idx;
+	int	idx;
 
 	idx = 0;
 	while (idx < info->num_philos)
 	{
-		if (info->end_flag != -1)
+		if (info->end_point != -1)
 		{
 			pthread_mutex_lock(info->philos[idx]->eat_count_mutex);
-			if (info->philos[idx]->eat_count >= info->end_flag)
+			if (info->philos[idx]->eat_count >= info->end_point)
 				*eat += 1;
 			pthread_mutex_unlock(info->philos[idx]->eat_count_mutex);
 		}
 		pthread_mutex_lock(info->philos[idx]->last_eaten_mutex);
-		if ((ph_now_ms(info) - philo->last_eaten) \
+		if ((ph_now_ms(info) - info->philos[idx]->last_eaten) \
 				>= info->time_to_die)
 		{
 			pthread_mutex_unlock(info->philos[idx]->last_eaten_mutex);
-			return (idx);
+			*dead_idx = idx;
+			return (1);
 		}
 		pthread_mutex_unlock(info->philos[idx]->last_eaten_mutex);
 		idx++;
+		usleep(100);
+	}
+	return (0);
+}
+
+static int	end_loops(t_program *info, int eat, int dead, int dead_idx)
+{
+	if (dead)
+	{
+		pthread_mutex_lock(info->dead_mutex);
+		pthread_mutex_lock(info->print_mutex);
+		info->end_flag = 1;
+		printf("%lu %d is died\n", ph_now_ms(info), dead_idx + 1);
+		pthread_mutex_unlock(info->print_mutex);
+		pthread_mutex_unlock(info->dead_mutex);
+		return (1);
+	}
+	if (eat == info->num_philos)
+	{
+		pthread_mutex_lock(info->dead_mutex);
+		info->end_flag = 1;
+		pthread_mutex_unlock(info->dead_mutex);
+		return (1);
 	}
 	return (0);
 }
 
 void	ph_monitoring(t_program *info)
 {
-	int eat;
+	int	eat;
 	int	dead;
+	int	dead_idx;
 
 	while (1)
 	{
 		eat = 0;
-		dead = check_end(info, &eat);
-		if (dead)
-		{
-			pthread_mutex_lock(info->print_mutex);
-			printf("%lu %d is died\n", ph_now_ms(info), dead + 1);
-			pthread_mutex_unlock(info->print_mutex);
+		dead_idx = 0;
+		dead = check_end(info, &eat, &dead_idx);
+		if (end_loops(info, eat, dead, dead_idx))
 			break ;
-		}
-		if (eat == info->num_philos)
-			break ;
-		break ;
+		usleep(100);
 	}
 }
