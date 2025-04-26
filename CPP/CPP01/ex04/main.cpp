3f5c1df1e3c538237df35fb0023dd4f9b481e1b7
Eumgill98 <wjdghcks02@gmail.com>
2025-04-27 01:27:15 +0900

    add : CPP01 ex04

--- CPP/CPP01/ex04/main.cpp
+++ CPP/CPP01/ex04/main.cpp
@@ -3,11 +3,26 @@
 
 int replace(char *infile, char *s1, char *s2, std::string buffer)
 {
-    std::fstream    outfile;
-    int             j;
+    std::fstream outfile;
+    std::string s1_str(s1);
+    std::string s2_str(s2);
 
-    outfile.open((std::string(infile) + ".replace").c_str(),ios::out);
-    // 
+    outfile.open((std::string(infile) + ".replace").c_str(), std::ios::out);
+    if (outfile.fail())
+        return 1;
+
+    for (int i = 0; i < (int)buffer.size(); i++) 
+    {
+        int j = buffer.find(s1_str, i);
+        if (j != -1 && j == i) {
+            outfile << s2_str;
+            i += s1_str.size() - 1;
+        } else {
+            outfile << buffer[i];
+        }
+    }
+    outfile.close();
+    return 0;
 }
 
 int main(int ac, char **av)
@@ -21,14 +36,14 @@ int main(int ac, char **av)
         std::cout << "argv must <file> <s1> <s2> !!" << std::endl;
         return (1); 
     }
-    infile.open(av[1], ios::in);
+    infile.open(av[1], std::ios::in);
     if (infile.fail())
     {
-        std::cout << "Error: " << av[1] << ":" << "no such file or diretory" << std::endl;
+        std::cout << "Error: " << av[1] << ":" << "no such file or directory" << std::endl;
         return (1);
     }
-    while (!inflie.eof() && infile >> std::noskipws >> c)
+    while (!infile.eof() && infile >> std::noskipws >> c)
         buffer += c;
-    infile.close()
+    infile.close();
     return (replace(av[1], av[2], av[3], buffer));
 }
